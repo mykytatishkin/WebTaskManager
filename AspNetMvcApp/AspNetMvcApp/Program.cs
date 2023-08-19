0df55a81d2968ed265de6b821d619bff43d06088
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-08-19 23:47:14 +0200

    connectionString test

--- AspNetMvcApp/AspNetMvcApp/Program.cs
+++ AspNetMvcApp/AspNetMvcApp/Program.cs
@@ -1,23 +1,35 @@
 ﻿using AspNetMvcApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+var services = builder.Services;
 
 // Configure the DbContext and add it to the services.
-var connectionString = builder.Configuration.GetConnectionString("Server=http://localhost:1433;Database=TaskManager;User=sa;Password=Password.1;"); // mb its my connection string, Im not sure
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); // Use the connection string name defined in your appsettings.json
+services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+// Add support for Entity Framework migrations
+// services.AddDatabaseDeveloperPageExceptionFilter();
+
+services.AddControllersWithViews();
+
 var app = builder.Build();
+
+// Apply database migrations (create/update the database)
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,7 +37,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
